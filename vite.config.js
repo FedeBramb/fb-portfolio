afe618dff42bac9bca4b7ea3f4893776745ad937
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-10-01 16:07:54 +0200

    Add base vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/fb-portfolio/',
+    base: '/fb-portfolio/src/main.jsx',
     plugins: [react()],
     css: {
         preprocessorOptions : {
