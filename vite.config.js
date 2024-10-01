17d357087682cc8f69fdd57a3b78bc0df7aea3c5
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-10-01 15:48:20 +0200

    Add base vite config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions : {
-        scss: {
-            api: "modern-compiler",
-        },
-    }
+    base: '/fb-portfolio/',
+    plugins: [react()],
+    css: {
+        preprocessorOptions : {
+            scss: {
+                api: "modern-compiler",
+            },
+        }
 },
 })
 
