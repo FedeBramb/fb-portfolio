c8ab948228adb1f78498d12755553069040b67f0
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-10-01 16:10:48 +0200

    Add _redirects

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/fb-portfolio/src/main.jsx',
     plugins: [react()],
     css: {
         preprocessorOptions : {
